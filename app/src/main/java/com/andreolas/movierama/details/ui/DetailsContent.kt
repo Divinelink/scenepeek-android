ae4c53c620ff774364de96b1aa92485aef956afd
Haris Andreolas <andreo.haris@gmail.com>
2023-01-26 12:07:59 +0200

    feat: add scaffold with loading state

--- app/src/main/java/com/andreolas/movierama/details/ui/DetailsContent.kt
+++ app/src/main/java/com/andreolas/movierama/details/ui/DetailsContent.kt
@@ -1,25 +1,87 @@
 package com.andreolas.movierama.details.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomSheetScaffold
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.rememberBottomSheetState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import com.andreolas.movierama.ExcludeFromJacocoGeneratedReport
 import com.andreolas.movierama.details.domain.model.Actor
 import com.andreolas.movierama.details.domain.model.Director
 import com.andreolas.movierama.details.domain.model.MovieDetails
+import com.andreolas.movierama.home.domain.model.PopularMovie
+import com.andreolas.movierama.home.ui.LoadingContent
 import com.andreolas.movierama.ui.UIText
 import com.andreolas.movierama.ui.theme.AppTheme
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsContent(
     viewState: DetailsViewState,
     modifier: Modifier = Modifier,
+    onNavigateUp: () -> Unit,
 ) {
-    // TODO
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState(
+        bottomSheetState = rememberBottomSheetState(
+            initialValue = BottomSheetValue.Collapsed,
+        )
+    )
+
+    BottomSheetScaffold(
+        sheetElevation = 32.dp,
+        sheetPeekHeight = 1.dp,
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        scaffoldState = bottomSheetScaffoldState,
+        sheetContent = {
+            // to do
+        },
+        modifier = Modifier
+            .navigationBarsPadding()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = viewState.movie.title,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onNavigateUp,
+                    ) {
+                        Icon(Icons.Filled.ArrowBack, null)
+                    }
+                },
+            )
+        }
+    ) { paddingValues ->
+        if (viewState is DetailsViewState.Initial) {
+            LoadingContent()
+        }
+    }
 }
 
 @Preview(
@@ -40,7 +102,9 @@ private fun DetailsContentPreview(
     AppTheme {
         Surface {
             DetailsContent(
+                modifier = Modifier,
                 viewState = viewState,
+                onNavigateUp = {},
             )
         }
     }
@@ -50,6 +114,15 @@ class DetailsViewStateProvider : PreviewParameterProvider<DetailsViewState> {
 
     override val values: Sequence<DetailsViewState>
         get() {
+            val popularMovie = PopularMovie(
+                id = 0,
+                posterPath = "",
+                releaseDate = "",
+                title = "Flight Club",
+                rating = "",
+                overview = "",
+                isFavorite = false,
+            )
             val movieDetails = MovieDetails(
                 id = 1123,
                 posterPath = "123456",
@@ -79,13 +152,17 @@ class DetailsViewStateProvider : PreviewParameterProvider<DetailsViewState> {
             )
 
             return sequenceOf(
-                DetailsViewState.Initial,
+                DetailsViewState.Initial(
+                    movie = popularMovie,
+                ),
 
                 DetailsViewState.Completed(
+                    movie = popularMovie,
                     movieDetails = movieDetails,
                 ),
 
                 DetailsViewState.Error(
+                    movie = popularMovie,
                     error = UIText.StringText("Something went wrong.")
                 ),
             )
--- app/src/main/java/com/andreolas/movierama/details/ui/DetailsScreen.kt
+++ app/src/main/java/com/andreolas/movierama/details/ui/DetailsScreen.kt
@@ -19,5 +19,8 @@ fun DetailsScreen(
 
     DetailsContent(
         viewState = viewState.value,
+        onNavigateUp = {
+            navigator.popBackStack()
+        }
     )
 }
