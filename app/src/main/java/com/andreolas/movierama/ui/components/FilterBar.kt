f0a65c0d7cdfcea92e038780ed76ac28ffe68b65
Haris Andreolas <andreo.haris@gmail.com>
2023-02-18 13:04:52 +0200

    feat: update filter bar animations

--- app/src/main/java/com/andreolas/movierama/ui/components/FilterBar.kt
+++ app/src/main/java/com/andreolas/movierama/ui/components/FilterBar.kt
@@ -1,27 +1,32 @@
 @file:Suppress("MagicNumber")
+
 package com.andreolas.movierama.ui.components
 
 import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,8 +36,10 @@ import com.andreolas.movierama.ui.theme.AppTheme
 /**
  * A composable that will display a list of filters.
  *
+ * @param modifier The modifier to be applied to the filter bar.
  * @param filters The list of filters to display.
- * @param onFilterClick A callback that will be called when a homeFilters is clicked.
+ * @param onFilterClick A callback that will be called when a filter is clicked.
+ * @param onClearClick A callback that will be called when the clear button is clicked.
  */
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -42,45 +49,43 @@ fun FilterBar(
     onFilterClick: (Filter) -> Unit,
     onClearClick: () -> Unit,
 ) {
-    val listState = rememberLazyListState()
-
-    LazyRow(
-        modifier = modifier,
-        state = listState,
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalAlignment = Alignment.CenterVertically,
+    CompositionLocalProvider(
+        LocalOverscrollConfiguration provides null
     ) {
-        val sortedFilters = filters.sortedBy { !it.isSelected }
-
-        if (sortedFilters.any { it.isSelected }) {
+        LazyRow(
+            modifier = modifier
+                .padding(start = 0.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            val sortedFilters = filters.sortedBy { !it.isSelected }
             item {
-                ClearButton(
-                    modifier = Modifier.animateItemPlacement(
-                        animationSpec = tween(
-                            durationMillis = 600,
+                AnimatedVisibility(visible = sortedFilters.any { it.isSelected }) {
+                    ClearButton(
+                        onClearClick = onClearClick,
+                    )
+                }
+            }
+
+            items(
+                count = sortedFilters.size,
+                key = { index -> sortedFilters[index].name },
+            ) { index ->
+                val filter = sortedFilters[index]
+                FilterItem(
+                    modifier = Modifier
+                        .animateItemPlacement(
+                            animationSpec = tween(
+                                durationMillis = 600,
+                            ),
                         ),
-                    ),
-                    onClearClick = onClearClick,
+                    filter = filter,
+                    onFilterClick = {
+                        onFilterClick(it)
+                    },
                 )
             }
-        }
-
-        items(
-            count = sortedFilters.size,
-            key = { index -> sortedFilters[index].name },
-        ) { index ->
-            val filter = sortedFilters[index]
-            FilterItem(
-                modifier = Modifier.animateItemPlacement(
-                    animationSpec = tween(
-                        durationMillis = 600,
-                    ),
-                ),
-                filter = filter,
-                onFilterClick = {
-                    onFilterClick(it)
-                },
-            )
         }
     }
 }
@@ -95,20 +100,20 @@ private fun ClearButton(
     modifier: Modifier = Modifier,
     onClearClick: () -> Unit,
 ) {
-    // This button should have a rounded corner.
-    val containerColor = MaterialTheme.colorScheme.surface
-    IconButton(
+    val containerColor = MaterialTheme.colorScheme.surfaceVariant
+    OutlinedButton(
+        onClick = onClearClick,
         modifier = modifier
-            .clip(RoundedCornerShape(100)),
-        colors = IconButtonDefaults.iconButtonColors(
+            .size(40.dp),
+        border = null,
+        shape = CircleShape,
+        contentPadding = PaddingValues(0.dp),
+        colors = ButtonDefaults.outlinedButtonColors(
             containerColor = containerColor,
             contentColor = contentColorFor(containerColor),
-        ),
-
-        onClick = onClearClick,
+        )
     ) {
         Icon(
-            modifier = modifier.clip(RoundedCornerShape(100)),
             imageVector = Icons.Default.Clear,
             contentDescription = stringResource(id = R.string.clear_filters_button_content_description),
         )
@@ -157,7 +162,9 @@ fun FilterBarPreview() {
 }
 
 /**
- * A data class that will hold the homeFilters name and if it is selected or not.
+ * A data class that represents a filter.
+ * @param name The name of the filter.
+ * @param isSelected Whether the filter is selected or not.
  */
 data class Filter(
     val name: String,
