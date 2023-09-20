ae7c88ac7e4eb55e6f2b1d31b251d40ce67da2ed
Harry Andreolas <andreo.haris@gmail.com>
2023-09-21 00:13:24 +0300

    feat: deserialize cast

--- app/src/main/java/com/andreolas/movierama/base/data/remote/movies/dto/details/credits/Cast.kt
+++ app/src/main/java/com/andreolas/movierama/base/data/remote/movies/dto/details/credits/Cast.kt
@@ -3,7 +3,7 @@ package com.andreolas.movierama.base.data.remote.movies.dto.details.credits
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Serializable
+@Serializable(with = CastSerializer::class)
 sealed class Cast {
   abstract val id: Int
   abstract val name: String
@@ -14,17 +14,17 @@ sealed class Cast {
   @Serializable
   data class Movie(
     val adult: Boolean,
-    @SerialName("cast_id") val castId: Int?,
-    override val character: String,
-    @SerialName("credit_id") val creditId: String,
     val gender: Int,
     override val id: Int,
+    @SerialName("cast_id") val castId: Int?,
     @SerialName("known_for_department") val knownForDepartment: String,
     override val name: String,
-    override val order: Int,
     @SerialName("original_name") val originalName: String,
-    @SerialName("profile_path") override val profilePath: String?,
     val popularity: Double,
+    @SerialName("profile_path") override val profilePath: String?,
+    override val character: String,
+    @SerialName("credit_id") val creditId: String,
+    override val order: Int,
   ) : Cast()
 
   @Serializable
@@ -34,8 +34,7 @@ sealed class Cast {
     override val id: Int,
     @SerialName("known_for_department") val knownForDepartment: String,
     override val name: String,
-    @SerialName("original_name")
-    val originalName: String,
+    @SerialName("original_name") val originalName: String,
     val popularity: Double,
     @SerialName("profile_path") override val profilePath: String?,
     override val character: String,
