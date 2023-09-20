ec55147fdc83fd655a78a8b2766613851d57dd01
Harry Andreolas <andreo.haris@gmail.com>
2023-09-20 23:48:12 +0300

    fix: cast polymorphic data classes

--- app/src/main/java/com/andreolas/movierama/base/data/remote/movies/dto/details/credits/Cast.kt
+++ app/src/main/java/com/andreolas/movierama/base/data/remote/movies/dto/details/credits/Cast.kt
@@ -4,40 +4,30 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed class Cast(
-  open val id: Int,
-  open val name: String,
-  open val profilePath: String?,
-  open val character: String,
-  open val order: Int,
-) {
+sealed class Cast {
+  abstract val id: Int
+  abstract val name: String
+  abstract val profilePath: String?
+  abstract val character: String
+  abstract val order: Int
 
+  @Serializable
   data class Movie(
     val adult: Boolean,
-    @SerialName("cast_id")
-    val castId: Int?,
+    @SerialName("cast_id") val castId: Int?,
     override val character: String,
-    @SerialName("credit_id")
-    val creditId: String,
+    @SerialName("credit_id") val creditId: String,
     val gender: Int,
     override val id: Int,
-    @SerialName("known_for_department")
-    val knownForDepartment: String,
+    @SerialName("known_for_department") val knownForDepartment: String,
     override val name: String,
     override val order: Int,
-    @SerialName("original_name")
-    val originalName: String,
+    @SerialName("original_name") val originalName: String,
+    @SerialName("profile_path") override val profilePath: String?,
     val popularity: Double,
-    @SerialName("profile_path")
-    override val profilePath: String?,
-  ) : Cast(
-    id = id,
-    name = name,
-    profilePath = profilePath ?: "",
-    character = character,
-    order = order,
-  )
+  ) : Cast()
 
+  @Serializable
   data class TV(
     val adult: Boolean,
     val gender: Int,
@@ -47,17 +37,9 @@ sealed class Cast(
     @SerialName("original_name")
     val originalName: String,
     val popularity: Double,
-    @SerialName("profile_path")
-    override val profilePath: String?,
+    @SerialName("profile_path") override val profilePath: String?,
     override val character: String,
-    @SerialName("credit_id")
-    val creditId: String,
+    @SerialName("credit_id") val creditId: String,
     override val order: Int,
-  ) : Cast(
-    id = id,
-    name = name,
-    profilePath = profilePath ?: "",
-    character = character,
-    order = order,
-  )
+  ) : Cast()
 }
--- app/src/main/java/com/andreolas/movierama/details/domain/model/Actor.kt
+++ app/src/main/java/com/andreolas/movierama/details/domain/model/Actor.kt
@@ -1,9 +1,9 @@
 package com.andreolas.movierama.details.domain.model
 
 data class Actor(
-    val id: Int,
-    val name: String,
-    val profilePath: String,
-    val character: String,
-    val order: Int,
+  val id: Int,
+  val name: String,
+  val profilePath: String,
+  val character: String,
+  val order: Int,
 )
