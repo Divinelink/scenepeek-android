801a7643d4a78d00f8d70fac9af506efe2f2397b
Harry Andreolas <harry.andreo@proton.me>
2024-06-02 15:19:28 +0300

    chore: remove unused helper classes

--- core-util/src/main/java/gr/divinelink/core/util/mapping/Factory.kt
+++
@@ -1,7 +0,0 @@
-package gr.divinelink.core.util.mapping
-
-import android.view.ViewGroup
-
-interface Factory<T : MappingModel> {
-    fun createViewHolder(parent: ViewGroup): MappingViewHolder<T>
-}
