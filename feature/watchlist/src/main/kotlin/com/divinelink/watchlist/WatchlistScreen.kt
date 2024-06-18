617ccaff444afc036a26a7447985c1782e8f2435
Harry Andreolas <harry.andreo@proton.me>
2024-06-18 16:30:48 +0300

    feat: add scroll behaviour on watchlist toolbar

--- feature/watchlist/src/main/kotlin/com/divinelink/watchlist/WatchlistScreen.kt
+++ feature/watchlist/src/main/kotlin/com/divinelink/watchlist/WatchlistScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -23,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -43,6 +47,8 @@ internal fun WatchlistScreen(
   navigator: DestinationsNavigator,
   viewModel: WatchlistViewModel = hiltViewModel()
 ) {
+  val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+
   var selectedPage by rememberSaveable { mutableIntStateOf(0) }
   val uiState = viewModel.uiState.collectAsState()
 
@@ -59,9 +65,15 @@ internal fun WatchlistScreen(
   }
 
   Scaffold(
-    modifier = Modifier.fillMaxSize(),
+    modifier = Modifier
+      .fillMaxSize()
+      .nestedScroll(scrollBehavior.nestedScrollConnection),
     topBar = {
       TopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+          scrolledContainerColor = MaterialTheme.colorScheme.surface,
+        ),
+        scrollBehavior = scrollBehavior,
         title = {
           Text(stringResource(R.string.feature_watchlist_title))
         }
