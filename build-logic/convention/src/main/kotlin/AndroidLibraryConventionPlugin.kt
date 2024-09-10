5c6bbba659f4dc24a379964f3c541a6c22e94891
Harry Andreolas <andreo.haris@gmail.com>
2024-09-10 16:48:49 +0300

    feat: add androidx tracing

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -1,8 +1,10 @@
 import com.android.build.gradle.LibraryExtension
 import com.divinelink.configureKotlinAndroid
+import com.divinelink.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
   override fun apply(target: Project) {
@@ -22,6 +24,10 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
         resourcePrefix =
           path.split("""\W""".toRegex()).drop(1).distinct().joinToString(separator = "_")
             .lowercase() + "_"
+      }
+
+      dependencies {
+        add("implementation", libs.findLibrary("androidx.tracing.ktx").get())
       }
     }
   }
