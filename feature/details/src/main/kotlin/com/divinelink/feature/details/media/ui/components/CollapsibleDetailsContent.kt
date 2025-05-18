f49bd7d9bb4d5d1f858a62ef7a640b7902484623
Harry Andreolas <harry@andreolas.slmail.me>
2025-05-18 20:57:41 +0300

    fix: set fixed media image size

--- feature/details/src/main/kotlin/com/divinelink/feature/details/media/ui/components/CollapsibleDetailsContent.kt
+++ feature/details/src/main/kotlin/com/divinelink/feature/details/media/ui/components/CollapsibleDetailsContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -66,14 +67,12 @@ fun CollapsibleDetailsContent(
       verticalAlignment = Alignment.CenterVertically,
     ) {
       MovieImage(
-        modifier = Modifier.weight(1.35f),
+        modifier = Modifier.height(MaterialTheme.dimensions.posterSize),
         path = mediaDetails.posterPath,
       )
 
       Column(
-        modifier = Modifier
-          .fillMaxSize()
-          .weight(3f),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
       ) {
         TitleDetails(mediaDetails = mediaDetails)
