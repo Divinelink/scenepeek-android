3834bb8f6523d9d820936f8e101ffc10e89a8323
Harry Andreolas <harry@andreolas.slmail.me>
2025-07-05 15:21:27 +0300

    test: add add to list screenshots

--- feature/add-to-account/src/main/kotlin/com/divinelink/feature/add/to/account/list/ui/AddToListContent.kt
+++ feature/add-to-account/src/main/kotlin/com/divinelink/feature/add/to/account/list/ui/AddToListContent.kt
@@ -2,12 +2,14 @@ package com.divinelink.feature.add.to.account.list.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,9 +17,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import com.divinelink.core.designsystem.theme.AppTheme
 import com.divinelink.core.designsystem.theme.LocalBottomNavigationPadding
 import com.divinelink.core.designsystem.theme.dimensions
 import com.divinelink.core.model.list.ListData
+import com.divinelink.core.ui.Previews
 import com.divinelink.core.ui.TestTags
 import com.divinelink.core.ui.blankslate.BlankSlate
 import com.divinelink.core.ui.components.DisplayMessageSection
@@ -25,66 +30,84 @@ import com.divinelink.core.ui.components.Material3CircularProgressIndicator
 import com.divinelink.feature.add.to.account.R
 import com.divinelink.feature.add.to.account.list.AddToListUiState
 import com.divinelink.feature.add.to.account.list.AddToListUserInteraction
+import com.divinelink.feature.add.to.account.list.ui.provider.AddToListUiStateParameterProvider
 
 @Composable
 fun AddToListContent(
   uiState: AddToListUiState,
   userInteraction: (AddToListUserInteraction) -> Unit,
 ) {
-  AnimatedVisibility(
-    visible = uiState.isLoading && uiState.lists is ListData.Data,
-    modifier = Modifier.fillMaxWidth(),
-  ) {
-    LinearProgressIndicator(
-      modifier = Modifier
-        .testTag(TestTags.LINEAR_LOADING_INDICATOR)
-        .fillMaxWidth(),
-    )
-  }
-
-  AnimatedVisibility(
-    modifier = Modifier.fillMaxWidth(),
-    visible = uiState.displayMessage != null,
-  ) {
-    DisplayMessageSection(
-      message = uiState.displayMessage,
-      onTimeout = { userInteraction(AddToListUserInteraction.ConsumeDisplayMessage) },
-    )
-  }
-
-  when {
-    uiState.error != null -> BlankSlate(
-      modifier = Modifier
-        .padding(horizontal = MaterialTheme.dimensions.keyline_16)
-        .padding(bottom = LocalBottomNavigationPadding.current),
-      uiState = uiState.error,
-    )
-
-    uiState.lists is ListData.Initial -> Box(
-      modifier = Modifier
-        .testTag(TestTags.LOADING_CONTENT)
-        .fillMaxWidth()
-        .height(MaterialTheme.dimensions.keyline_96),
+  Column {
+    AnimatedVisibility(
+      visible = uiState.isLoading && uiState.lists is ListData.Data,
+      modifier = Modifier.fillMaxWidth(),
     ) {
-      Material3CircularProgressIndicator(
+      LinearProgressIndicator(
         modifier = Modifier
-          .wrapContentSize()
-          .align(Alignment.Center),
+          .testTag(TestTags.LINEAR_LOADING_INDICATOR)
+          .fillMaxWidth(),
       )
     }
 
-    uiState.lists is ListData.Data && uiState.lists.isEmpty -> Text(
-      modifier = Modifier
-        .fillMaxWidth()
-        .padding(MaterialTheme.dimensions.keyline_32),
-      text = stringResource(R.string.feature_add_to_account_empty_lists),
-      textAlign = TextAlign.Center,
-      style = MaterialTheme.typography.bodyLarge,
-    )
+    AnimatedVisibility(
+      modifier = Modifier.fillMaxWidth(),
+      visible = uiState.displayMessage != null,
+    ) {
+      DisplayMessageSection(
+        message = uiState.displayMessage,
+        onTimeout = { userInteraction(AddToListUserInteraction.ConsumeDisplayMessage) },
+      )
+    }
 
-    uiState.lists is ListData.Data -> ListsDataContent(
-      data = uiState.lists.data,
-      userInteraction = userInteraction,
-    )
+    when {
+      uiState.error != null -> BlankSlate(
+        modifier = Modifier
+          .padding(horizontal = MaterialTheme.dimensions.keyline_16)
+          .padding(bottom = LocalBottomNavigationPadding.current),
+        uiState = uiState.error,
+      )
+
+      uiState.lists is ListData.Initial -> Box(
+        modifier = Modifier
+          .testTag(TestTags.LOADING_CONTENT)
+          .fillMaxWidth()
+          .height(MaterialTheme.dimensions.keyline_96),
+      ) {
+        Material3CircularProgressIndicator(
+          modifier = Modifier
+            .wrapContentSize()
+            .align(Alignment.Center),
+        )
+      }
+
+      uiState.lists is ListData.Data && uiState.lists.isEmpty -> Text(
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(MaterialTheme.dimensions.keyline_32),
+        text = stringResource(R.string.feature_add_to_account_empty_lists),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodyLarge,
+      )
+
+      uiState.lists is ListData.Data -> ListsDataContent(
+        data = uiState.lists.data,
+        userInteraction = userInteraction,
+      )
+    }
+  }
+}
+
+@Composable
+@Previews
+fun AddToListContentPreview(
+  @PreviewParameter(AddToListUiStateParameterProvider::class) state: AddToListUiState,
+) {
+  AppTheme {
+    Surface {
+      AddToListContent(
+        uiState = state,
+        userInteraction = { },
+      )
+    }
   }
 }
