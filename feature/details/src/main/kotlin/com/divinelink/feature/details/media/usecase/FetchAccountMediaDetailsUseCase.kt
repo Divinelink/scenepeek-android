7d214e231e3296c8f5a0cba4528ed23fc0632d8c
Harry Andreolas <harry@andreolas.slmail.me>
2025-08-11 20:05:05 +0300

    feat: observe tmdb account when fetching account media details

--- feature/details/src/main/kotlin/com/divinelink/feature/details/media/usecase/FetchAccountMediaDetailsUseCase.kt
+++ feature/details/src/main/kotlin/com/divinelink/feature/details/media/usecase/FetchAccountMediaDetailsUseCase.kt
@@ -5,9 +5,9 @@ import com.divinelink.core.commons.domain.FlowUseCase
 import com.divinelink.core.commons.domain.data
 import com.divinelink.core.data.details.model.MediaDetailsParams
 import com.divinelink.core.data.details.repository.DetailsRepository
-import com.divinelink.core.model.exception.SessionException
 import com.divinelink.core.datastore.SessionStorage
 import com.divinelink.core.model.account.AccountMediaDetails
+import com.divinelink.core.model.exception.SessionException
 import com.divinelink.core.model.media.MediaType
 import com.divinelink.core.network.media.model.states.AccountMediaDetailsRequestApi
 import kotlinx.coroutines.flow.Flow
@@ -17,30 +17,30 @@ open class FetchAccountMediaDetailsUseCase(
   private val sessionStorage: SessionStorage,
   private val repository: DetailsRepository,
   val dispatcher: DispatcherProvider,
-) : FlowUseCase<MediaDetailsParams, AccountMediaDetails>(dispatcher.io) {
+) : FlowUseCase<MediaDetailsParams, AccountMediaDetails>(dispatcher.default) {
   override fun execute(parameters: MediaDetailsParams): Flow<Result<AccountMediaDetails>> = flow {
-    val sessionId = sessionStorage.sessionId
+    sessionStorage.accountStorage.accountId.collect {
+      val sessionId = sessionStorage.sessionId
+      if (sessionId == null) {
+        emit(Result.failure(SessionException.Unauthenticated()))
+      } else {
+        val request = when (parameters.mediaType) {
+          MediaType.MOVIE -> AccountMediaDetailsRequestApi.Movie(
+            movieId = parameters.id,
+            sessionId = sessionId,
+          )
+          MediaType.TV -> AccountMediaDetailsRequestApi.TV(
+            seriesId = parameters.id,
+            sessionId = sessionId,
+          )
+          else -> throw IllegalArgumentException("Unsupported media type: ${parameters.mediaType}")
+        }
 
-    if (sessionId == null) {
-      emit(Result.failure(SessionException.Unauthenticated()))
-      return@flow
-    } else {
-      val request = when (parameters.mediaType) {
-        MediaType.MOVIE -> AccountMediaDetailsRequestApi.Movie(
-          movieId = parameters.id,
-          sessionId = sessionId,
-        )
-        MediaType.TV -> AccountMediaDetailsRequestApi.TV(
-          seriesId = parameters.id,
-          sessionId = sessionId,
-        )
-        else -> throw IllegalArgumentException("Unsupported media type: ${parameters.mediaType}")
-      }
-
-      repository.fetchAccountMediaDetails(
-        request = request,
-      ).collect {
-        emit(Result.success(it.data))
+        repository.fetchAccountMediaDetails(
+          request = request,
+        ).collect {
+          emit(Result.success(it.data))
+        }
       }
     }
   }
