a2e45ae0565ca5a125bb320925f614e4249318f0
Harry Andreolas <harry@andreolas.slmail.me>
2025-09-16 22:26:59 +0300

    feat: parse requester id on domain model

--- core/model/src/main/kotlin/com/divinelink/core/model/jellyseerr/media/JellyseerrRequester.kt
+++ core/model/src/main/kotlin/com/divinelink/core/model/jellyseerr/media/JellyseerrRequester.kt
@@ -1,3 +1,6 @@
 package com.divinelink.core.model.jellyseerr.media
 
-data class JellyseerrRequester(val displayName: String)
+data class JellyseerrRequester(
+  val id: Int,
+  val displayName: String,
+)
