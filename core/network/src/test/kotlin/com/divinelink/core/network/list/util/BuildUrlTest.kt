c73a752547282f579eae94f806489c9237a6269d
Harry Andreolas <harry@andreolas.slmail.me>
2025-09-25 19:19:40 +0300

    test: validate buildUrl functions

--- core/network/src/test/kotlin/com/divinelink/core/network/list/util/BuildUrlTest.kt
+++ core/network/src/test/kotlin/com/divinelink/core/network/list/util/BuildUrlTest.kt
@@ -1,16 +1,32 @@
 package com.divinelink.core.network.list.util
 
-import com.google.common.truth.Truth.assertThat
+import com.divinelink.core.fixtures.model.media.MediaItemFactory
+import com.divinelink.core.model.media.toStub
+import io.kotest.matchers.shouldBe
 import kotlin.test.Test
 
 class BuildUrlTest {
 
   @Test
   fun `test buildAddItemsToListUrl`() {
-    assertThat(
-      buildListItemsUrl(12345),
-    ).isEqualTo(
-      "https://api.themoviedb.org/4/list/12345/items",
-    )
+    buildListItemsUrl(12345) shouldBe "https://api.themoviedb.org/4/list/12345/items"
+  }
+
+  @Test
+  fun `test buildListUrl`() {
+    buildListUrl() shouldBe "https://api.themoviedb.org/4/list"
+  }
+
+  @Test
+  fun `test buildListWithIdUrl`() {
+    buildListWithIdUrl(12345) shouldBe "https://api.themoviedb.org/4/list/12345"
+  }
+
+  @Test
+  fun `test buildListItemStatusUrl`() {
+    buildListItemStatusUrl(
+      listId = 12345,
+      media = MediaItemFactory.bruceAlmighty().toStub(),
+    ) shouldBe "https://api.themoviedb.org/4/list/12345/item_status?media_id=310&media_type=movie"
   }
 }
