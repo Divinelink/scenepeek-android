acff0df927238935e9722a1dfe3d4077ac7e8c20
Harry Andreolas <harry@andreolas.slmail.me>
2025-12-02 21:31:51 +0200

    chore: update pre-push to execute linter

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 echo "Check code changes..."
-OUTPUT="/tmp/pre-push-unit-tests-$(date +%s)"
-#./gradlew RunUnitTests > $OUTPUT
+OUTPUT="/tmp/pre-push-static-analysis-$(date +%s)"
+./gradlew ktlintCheck > $OUTPUT
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
   cat $OUTPUT
   rm $OUTPUT
   echo "***********************************************"
-  echo "            Unit tests failed              "
+  echo "            Static analysis failed             "
   echo " Please fix the above issues before committing "
   echo "***********************************************"
   exit $EXIT_CODE
