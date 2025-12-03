a567743904e009542a6092dc0880fd316923943b
Harry Andreolas <harry@andreolas.slmail.me>
2025-12-03 20:56:33 +0200

    feat: update ios app versions

--- core/commons/src/nativeMain/kotlin/com/divinelink/core/commons/provider/BuildConfigProvider.native.kt
+++ core/commons/src/nativeMain/kotlin/com/divinelink/core/commons/provider/BuildConfigProvider.native.kt
@@ -1,10 +1,19 @@
 package com.divinelink.core.commons.provider
 
+import platform.Foundation.NSBundle
+import kotlin.experimental.ExperimentalNativeApi
+
+private val mainBundle = NSBundle.mainBundle
+private val CFBundleShortVersionString = mainBundle
+  .objectForInfoDictionaryKey("CFBundleShortVersionString") as? String
+private val CFBundleVersion = mainBundle.objectForInfoDictionaryKey("CFBundleVersion") as? String
+
+@OptIn(ExperimentalNativeApi::class)
 class NativeBuildConfigProvider(
-  override val versionName: String = "1.2.3",
-  override val buildType: String = "debug",
-  override val isDebug: Boolean = true,
-  override val versionCode: Int = 1,
+  override val versionName: String = CFBundleShortVersionString ?: "Unknown",
+  override val isDebug: Boolean = Platform.isDebugBinary,
+  override val buildType: String = if (isDebug) "debug" else "release",
+  override val versionCode: Int = CFBundleVersion?.toIntOrNull() ?: 0,
   override val versionData: String = buildString {
     append("iOS ")
     append(versionName)
