338aa08da7f63530b3629cac9635950d58bde43a
Harry Andreolas <harry@andreolas.slmail.me>
2026-01-04 17:38:06 +0200

    feat: set grid as default viewMode for search

--- core/datastore/src/androidHostTest/kotlin/com/divinelink/core/datastore/ui/DatastoreUiStorageTest.kt
+++ core/datastore/src/androidHostTest/kotlin/com/divinelink/core/datastore/ui/DatastoreUiStorageTest.kt
@@ -5,7 +5,7 @@ import com.divinelink.core.model.ui.UiPreferences
 import com.divinelink.core.model.ui.ViewMode
 import com.divinelink.core.model.ui.ViewableSection
 import com.divinelink.core.testing.datastore.TestDatastoreFactory
-import com.google.common.truth.Truth.assertThat
+import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.test.runTest
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -23,10 +23,18 @@ class DatastoreUiStorageTest {
     storage = DatastoreUiStorage(testDataStore)
 
     storage.uiPreferences.test {
-      assertThat(awaitItem()).isEqualTo(
-        UiPreferences.Initial.copy(
-          viewModes = ViewableSection.entries.associateWith { ViewMode.LIST },
-        ),
+      awaitItem() shouldBe UiPreferences.Initial.copy(
+        viewModes = ViewableSection.entries.associateWith { section ->
+          when (section) {
+            ViewableSection.LISTS -> ViewMode.LIST
+            ViewableSection.PERSON_CREDITS -> ViewMode.LIST
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.GRID
+          }
+        },
       )
     }
   }
@@ -36,50 +44,54 @@ class DatastoreUiStorageTest {
     storage = DatastoreUiStorage(testDataStore)
 
     storage.uiPreferences.test {
-      assertThat(awaitItem()).isEqualTo(
-        UiPreferences.Initial.copy(
-          viewModes = ViewableSection.entries.associateWith { ViewMode.LIST },
-        ),
+      awaitItem() shouldBe UiPreferences.Initial.copy(
+        viewModes = ViewableSection.entries.associateWith { section ->
+          when (section) {
+            ViewableSection.LISTS -> ViewMode.LIST
+            ViewableSection.PERSON_CREDITS -> ViewMode.LIST
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.GRID
+          }
+        },
       )
 
       storage.updateViewMode(
         section = ViewableSection.PERSON_CREDITS,
       )
 
-      assertThat(awaitItem()).isEqualTo(
-        UiPreferences.Initial.copy(
-          viewModes = ViewableSection.entries.associateWith {
-            when (it) {
-              ViewableSection.LISTS -> ViewMode.LIST
-              ViewableSection.PERSON_CREDITS -> ViewMode.GRID
-              ViewableSection.DISCOVER -> ViewMode.LIST
-              ViewableSection.USER_DATA -> ViewMode.LIST
-              ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
-              ViewableSection.LIST_DETAILS -> ViewMode.LIST
-              ViewableSection.SEARCH -> ViewMode.LIST
-            }
-          },
-        ),
+      awaitItem() shouldBe UiPreferences.Initial.copy(
+        viewModes = ViewableSection.entries.associateWith {
+          when (it) {
+            ViewableSection.LISTS -> ViewMode.LIST
+            ViewableSection.PERSON_CREDITS -> ViewMode.GRID
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.LIST
+          }
+        },
       )
 
       storage.updateViewMode(
         section = ViewableSection.LISTS,
       )
 
-      assertThat(awaitItem()).isEqualTo(
-        UiPreferences.Initial.copy(
-          viewModes = ViewableSection.entries.associateWith {
-            when (it) {
-              ViewableSection.LISTS -> ViewMode.GRID
-              ViewableSection.PERSON_CREDITS -> ViewMode.GRID
-              ViewableSection.DISCOVER -> ViewMode.LIST
-              ViewableSection.USER_DATA -> ViewMode.LIST
-              ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
-              ViewableSection.LIST_DETAILS -> ViewMode.LIST
-              ViewableSection.SEARCH -> ViewMode.LIST
-            }
-          },
-        ),
+      awaitItem() shouldBe UiPreferences.Initial.copy(
+        viewModes = ViewableSection.entries.associateWith {
+          when (it) {
+            ViewableSection.LISTS -> ViewMode.GRID
+            ViewableSection.PERSON_CREDITS -> ViewMode.GRID
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.GRID
+          }
+        },
       )
     }
   }
--- core/model/src/commonMain/kotlin/com/divinelink/core/model/ui/UiPreferences.kt
+++ core/model/src/commonMain/kotlin/com/divinelink/core/model/ui/UiPreferences.kt
@@ -3,7 +3,17 @@ package com.divinelink.core.model.ui
 data class UiPreferences(val viewModes: Map<ViewableSection, ViewMode>) {
   companion object {
     val Initial = UiPreferences(
-      viewModes = ViewableSection.entries.associateWith { ViewMode.LIST },
+      viewModes = ViewableSection.entries.associateWith { section ->
+        when (section) {
+          ViewableSection.LISTS -> ViewMode.LIST
+          ViewableSection.PERSON_CREDITS -> ViewMode.LIST
+          ViewableSection.DISCOVER -> ViewMode.LIST
+          ViewableSection.USER_DATA -> ViewMode.LIST
+          ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+          ViewableSection.LIST_DETAILS -> ViewMode.LIST
+          ViewableSection.SEARCH -> ViewMode.GRID
+        }
+      },
     )
   }
 }
