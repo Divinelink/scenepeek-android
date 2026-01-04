76fbb5cca2554b0e780cadf164b5f296f25b3738
Harry Andreolas <harry@andreolas.slmail.me>
2026-01-04 16:25:59 +0200

    feat: add viewable section preference for search

---
+++ core/data/src/commonTest/kotlin/com/divinelink/core/data/preferences/ProdPreferencesRepositoryTest.kt
@@ -0,0 +1,71 @@
+package com.divinelink.core.data.preferences
+
+import app.cash.turbine.test
+import com.divinelink.core.model.ui.UiPreferences
+import com.divinelink.core.model.ui.ViewMode
+import com.divinelink.core.model.ui.ViewableSection
+import com.divinelink.core.testing.storage.TestUiSettingsStorage
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+
+class ProdPreferencesRepositoryTest {
+
+  private val repository = ProdPreferencesRepository(
+    storage = TestUiSettingsStorage(),
+  )
+
+  @Test
+  fun `test default view modes`() = runTest {
+    repository.uiPreferences.test {
+      awaitItem() shouldBe UiPreferences(
+        viewModes = ViewableSection.entries.associateWith { ViewMode.LIST },
+      )
+    }
+  }
+
+  @Test
+  fun `test update ViewMode`() = runTest {
+    repository.uiPreferences.test {
+      awaitItem() shouldBe UiPreferences(
+        viewModes = ViewableSection.entries.associateWith { ViewMode.LIST },
+      )
+
+      repository.switchViewMode(
+        section = ViewableSection.PERSON_CREDITS,
+      )
+
+      awaitItem() shouldBe UiPreferences(
+        viewModes = ViewableSection.entries.associateWith {
+          when (it) {
+            ViewableSection.LISTS -> ViewMode.LIST
+            ViewableSection.PERSON_CREDITS -> ViewMode.GRID
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.LIST
+          }
+        },
+      )
+
+      repository.switchViewMode(
+        section = ViewableSection.LISTS,
+      )
+
+      awaitItem() shouldBe UiPreferences(
+        viewModes = ViewableSection.entries.associateWith {
+          when (it) {
+            ViewableSection.LISTS -> ViewMode.GRID
+            ViewableSection.PERSON_CREDITS -> ViewMode.GRID
+            ViewableSection.DISCOVER -> ViewMode.LIST
+            ViewableSection.USER_DATA -> ViewMode.LIST
+            ViewableSection.MEDIA_DETAILS -> ViewMode.LIST
+            ViewableSection.LIST_DETAILS -> ViewMode.LIST
+            ViewableSection.SEARCH -> ViewMode.LIST
+          }
+        },
+      )
+    }
+  }
+}
