04e6b6885d45b41780653fb1495cae70236e7ed1
Harry Andreolas <25794786+Divinelink@users.noreply.github.com>
2026-03-13 19:56:31 +0200

    feat: handle universal links in ios app

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -4,19 +4,36 @@ import app
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         InitializeKt.setup(isDebug: isDebug())
-        
+
+        if let url = launchOptions?[.url] as? URL {
+            ExternalUriHandler.shared.onNewUri(uri: url.absoluteString)
+        }
+
         window = UIWindow(frame: UIScreen.main.bounds)
         if let window = window {
             window.rootViewController = MainKt.MainViewController()
             window.makeKeyAndVisible()
         }
         return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb,
+           let url = userActivity.webpageURL {
+            ExternalUriHandler.shared.onNewUri(uri: url.absoluteString)
+            return true
+        }
+        return false
     }
 
     func application(
