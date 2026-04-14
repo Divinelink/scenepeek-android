3848e36dff52248b9453c287288f289b44cbaeef
Harry Andreolas <25794786+Divinelink@users.noreply.github.com>
2026-04-14 21:27:55 +0300

    feat: update animation spec

--- core/scaffold/src/commonMain/kotlin/com/divinelink/core/scaffold/ScenePeekNavHost.kt
+++ core/scaffold/src/commonMain/kotlin/com/divinelink/core/scaffold/ScenePeekNavHost.kt
@@ -1,5 +1,10 @@
 package com.divinelink.core.scaffold
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
@@ -28,5 +33,14 @@ fun ScenePeekNavHost() {
       then
       SinglePaneSceneStrategy(),
     entryProvider = koinEntryProvider(),
+    transitionSpec = { navigationSpec() },
+    popTransitionSpec = { navigationSpec() },
+    predictivePopTransitionSpec = { navigationSpec() },
   )
 }
+
+private fun navigationSpec() = fadeIn(
+  animationSpec = tween(durationMillis = 300, easing = LinearEasing),
+) togetherWith fadeOut(
+  animationSpec = tween(durationMillis = 300, easing = LinearEasing),
+)
